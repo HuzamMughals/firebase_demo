3a6ee43de63a45a100a5ec0e35a0b02949deb79a
MUHAMMAD ILYAS <mail.ilyas@yahoo.com>
2021-06-07 00:01:19 +0500

    project init

---
+++ config.js
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: "AIzaSyD-wx4mqdm2BSEqEc_q3Bp-U6wOcxICubw",
+  authDomain: "upwork-firebase-4b346.firebaseapp.com",
+  projectId: "upwork-firebase-4b346",
+  storageBucket: "upwork-firebase-4b346.appspot.com",
+  messagingSenderId: "365626907046",
+  appId: "1:365626907046:web:790a77c54960436ff29767",
+};
