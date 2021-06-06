1c12d5b403ee5c8a38add970c5ac128f1017b99b
MUHAMMAD ILYAS <mail.ilyas@yahoo.com>
2021-06-07 01:07:15 +0500

    component updated

--- index.js
+++ index.js
@@ -1,66 +1,91 @@
-import * as React from "react";
+import React, { useState, useRef } from "react";
 import { render } from "react-dom";
 import firebase from "firebase/app";
-import "firebase/database";
+import "firebase/firestore";
+
 import {
-  FirebaseDatabaseProvider,
-  FirebaseDatabaseNode,
-  FirebaseDatabaseTransaction,
-} from "@react-firebase/database";
+  FirestoreProvider,
+  FirestoreMutation,
+  FirestoreCollection,
+} from "@react-firebase/firestore";
+
 import { config } from "./config";
 
-const styles = {
-  fontFamily: "sans-serif",
+const App = () => {
+  const [name, setName] = useState("");
+  const [date, setDate] = useState("");
+  const nameRef = useRef();
+  nameRef.current = name;
+
+  const dateRef = useRef();
+  dateRef.current = date;
+
+  return (
+    <div>
+      Upwork Demo Firebase:
+      <br />
+      <br />
+      <br />
+      <label>
+        Name:
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <label>
+        Date:
+        <input
+          type="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
+      </label>
+      <br />
+      <br />
+      <FirestoreProvider firebase={firebase} {...config}>
+        <FirestoreMutation type="add" path="/upwork-form">
+          {({ runMutation }) => {
+            return (
+              <div>
+                <button
+                  onClick={() => {
+                    if (!dateRef.current || !nameRef.current) return;
+                    runMutation({
+                      date: dateRef.current,
+                      name: nameRef.current,
+                    }).then((res) => {
+                      console.log("Ran mutation ", res);
+                    });
+                  }}
+                >
+                  Save Record
+                </button>
+              </div>
+            );
+          }}
+        </FirestoreMutation>
+
+        <FirestoreCollection path="/upwork-form">
+          {(d) => {
+            return (
+              <table>
+                <tbody>
+                  {d.value?.map((item, key) => (
+                    <tr key={key}>
+                      <td>{item.date}</td>
+                      <td>{item.name}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            );
+          }}
+        </FirestoreCollection>
+      </FirestoreProvider>
+    </div>
+  );
 };
-
-const stringify = (a) => JSON.stringify(a, null, 2);
-
-const path = "user_bookmarks/a/usage_count";
-
-const App = () => (
-  <div style={styles}>
-    <FirebaseDatabaseProvider firebase={firebase} {...config}>
-      <FirebaseDatabaseTransaction path={path}>
-        {({ runTransaction }) => {
-          return (
-            <div>
-              <button
-                onClick={() => {
-                  runTransaction({
-                    reducer: (val) => {
-                      if (val === null) {
-                        return 1;
-                      } else {
-                        return val + 1;
-                      }
-                    },
-                  }).then(() => {
-                    console.log("Ran transaction");
-                  });
-                }}
-              >
-                Click me to run transaction
-              </button>
-            </div>
-          );
-        }}
-      </FirebaseDatabaseTransaction>
-
-      <FirebaseDatabaseNode path={`${path}`}>
-        {(d) => {
-          return (
-            <div>
-              {d.value && (
-                <pre data-testid="test-value">Value : {s(d.value)}</pre>
-              )}
-              {d.path && <pre data-testid="test-path">Path : {d.path}</pre>}
-              <pre data-testid="test-is-loading">isLoading : {d.isLoading}</pre>
-            </div>
-          );
-        }}
-      </FirebaseDatabaseNode>
-    </FirebaseDatabaseProvider>
-  </div>
-);
 
 render(<App />, document.getElementById("root"));
